e8ba3f571178490dd8b86cc22c697bd4bc91a71a
Gustavo <gmassamichi@gmail.com>
2024-04-04 22:23:21 -0300

    adição de filtro e botões em telas-reservadas

--- src/app/tela-reservas-feitas/tela-reservas-feitas.component.ts
+++ src/app/tela-reservas-feitas/tela-reservas-feitas.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { TelaLoginCadastroComponent } from '../tela-login-cadastro/tela-login-cadastro.component';
 import { SalaDataService } from 'src/app/services/sala-data.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatTable } from '@angular/material/table'; // Removed MatTableModule as it's not necessary here
+import { MatButtonModule } from '@angular/material/button';
+
+interface Sala {
+  numero: number;
+  professor: string;
+  materia: string;
+  dia: Date;
+}
 
 @Component({
   selector: 'app-tela-reservas-feitas',
@@ -11,6 +21,7 @@ import { SalaDataService } from 'src/app/services/sala-data.service';
 export class TelaReservasFeitasComponent {
   salas: Sala[] = [];
   displayedColumns: string[] = ['numero', 'professor', 'materia', 'dia'];
+  dataSource = new MatTableDataSource<Sala>(this.salas);
 
   constructor(
     private salaDataService: SalaDataService,
@@ -18,6 +29,7 @@ export class TelaReservasFeitasComponent {
   ) {
     this.salaDataService.salaReservaData$.subscribe((salas) => {
       this.salas = salas;
+      this.dataSource.data = this.salas; // Atualize o dataSource.data com os novos dados
     });
   }
 
@@ -28,11 +40,25 @@ export class TelaReservasFeitasComponent {
       console.log(`Dialog result: ${result}`);
     });
   }
-}
 
-interface Sala {
-  numero: number;
-  professor: string;
-  materia: string;
-  dia: Date;
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+  
+  @ViewChild(MatTable)
+  table!: MatTable<Sala>;
+
+  addData() {
+    const randomElementIndex = Math.floor(Math.random() * this.salas.length);
+    this.salas.push(this.salas[randomElementIndex]); // Adiciona um elemento aleatório ao array
+    this.dataSource.data = this.salas; // Atualiza o dataSource com os novos dados
+    this.table.renderRows(); // Renderiza as linhas da tabela
+  }
+
+  removeData() {
+    this.salas.pop(); // Remove o último elemento do array
+    this.dataSource.data = this.salas; // Atualiza o dataSource com os novos dados
+    this.table.renderRows(); // Renderiza as linhas da tabela
+  }
 }
