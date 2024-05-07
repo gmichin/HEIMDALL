d4a97f6d0b10cb7fac2d12d7e636e79ef5a7f068
Gustavo <gmassamichi@gmail.com>
2024-05-07 17:59:50 -0300

    save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -45,6 +45,7 @@ import { TelaSalasComponent } from './tela-salas/tela-salas.component'
 import { TelaDeletarSalasComponent } from './tela-salas/tela-deletar-salas/tela-deletar-salas.component';
 import { TelaPermissaoSalasComponent } from './tela-salas/tela-permissao-salas/tela-permissao-salas.component';
 import { TelaSalasFeitasComponent } from './tela-salas/tela-salas-feitas/tela-salas-feitas.component';
+import { TelaNovasSalasComponent } from './tela-salas/tela-novas-salas/tela-novas-salas.component';
 
 @NgModule({
   declarations: [
@@ -62,7 +63,8 @@ import { TelaSalasFeitasComponent } from './tela-salas/tela-salas-feitas/tela-sa
     TelaSalasComponent,
     TelaDeletarSalasComponent,
     TelaPermissaoSalasComponent,
-    TelaSalasFeitasComponent
+    TelaSalasFeitasComponent,
+    TelaNovasSalasComponent
   ],
   imports: [
     AppRoutingModule,
--- src/app/tela-salas/tela-novas-salas/tela-novas-salas.component.ts
+++ src/app/tela-salas/tela-novas-salas/tela-novas-salas.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { TelaLoginCadastroComponent } from 'src/app/tela-login-cadastro/tela-login-cadastro.component';
+import { TelaReservasComponent } from 'src/app/tela-reservas/tela-reservas.component';
+import { TelaSalasComponent } from '../tela-salas.component';
 
 @Component({
   selector: 'app-tela-novas-salas',
-  standalone: true,
-  imports: [],
   templateUrl: './tela-novas-salas.component.html',
   styleUrl: './tela-novas-salas.component.scss'
 })
 export class TelaNovasSalasComponent {
+  constructor(public dialog: MatDialog) {}
 
+  openLoginSignUp() {
+    const dialogRef = this.dialog.open(TelaLoginCadastroComponent);
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+  openReservas() {
+    const dialogRef = this.dialog.open(TelaReservasComponent);
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+  openSalas() {
+    const dialogRef = this.dialog.open(TelaSalasComponent);
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
