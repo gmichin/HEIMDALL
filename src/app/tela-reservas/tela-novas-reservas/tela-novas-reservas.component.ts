be5f4600d2c3d730c846fa1a19592be15a293b7f
Gustavo <gmassamichi@gmail.com>
2024-05-09 16:59:59 -0300

    save

--- src/app/tela-reservas/tela-novas-reservas/tela-novas-reservas.component.ts
+++ src/app/tela-reservas/tela-novas-reservas/tela-novas-reservas.component.ts
@@ -286,7 +286,7 @@ export class TelaNovasReservasComponent implements OnInit{
           course: course.name,
           instituition: course.instituition
         }
-        
+        this.materiasPorProfessor.push(prof);
       })
     })
 
