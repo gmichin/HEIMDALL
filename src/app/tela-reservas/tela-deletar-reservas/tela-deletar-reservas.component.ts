eb0707e0bac548861b3d34cdd48d1eb29810b050
Gustavo <gmassamichi@gmail.com>
2024-05-13 20:24:50 -0300

    save

--- src/app/tela-reservas/tela-deletar-reservas/tela-deletar-reservas.component.ts
+++ src/app/tela-reservas/tela-deletar-reservas/tela-deletar-reservas.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { TelaLoginCadastroComponent } from 'src/app/tela-login-cadastro/tela-login-cadastro.component';
 import { TelaReservasComponent } from '../tela-reservas.component';
 import { SalaDataService } from 'src/app/services/sala-data.service';
 import { SelectionModel } from '@angular/cdk/collections';
 import { TelaSalasComponent } from 'src/app/tela-salas/tela-salas.component';
-import { map } from 'rxjs/operators';
 
 interface Sala {
   _id: string;
@@ -35,32 +35,14 @@ export class TelaDeletarReservasComponent {
   displayedColumns: string[] = ['selecionar','numero', 'professor', 'materia', 'dia', 'remove'];
 
   selection = new SelectionModel<any>(true, []);
-  numerosSalasEncontradas: any[] | undefined;
 
   constructor(
     public dialog: MatDialog,
+    private router: Router,
     private salaDataService: SalaDataService
   ) {
     this.salaDataService.salaReservaData$.subscribe((salas) => {
-      const salaIds = salas.map((sala) => sala.room_id);
-      this.salaDataService.salaData$.pipe(
-        map((salaData) => {
-          const numerosSalasEncontradas = [];
-          for (const salaId of salaIds) {
-            const salaEncontrada = salaData.find((sala) => sala._id === salaId);
-            if (salaEncontrada) {
-              numerosSalasEncontradas.push(salaEncontrada.number);
-            }
-          }
-          return numerosSalasEncontradas;
-        })
-      ).subscribe((numerosSalasEncontradas) => {
-        // Aqui, numerosSalasEncontradas é uma matriz que contém os números das salas encontradas
-        console.log(numerosSalasEncontradas); // Verifique se os números das salas foram encontrados corretamente
-        // Faça o que precisar com os números das salas encontradas
-        // Por exemplo, você pode atribuir esta matriz a uma variável em sua classe e usá-la na interface do usuário
-        this.numerosSalasEncontradas = numerosSalasEncontradas;
-      });
+      this.numeroSala = salas.map((sala) => sala.room_id);
     });
     this.salaDataService.salaReservaData$.subscribe((salas) => {
       this.professorNomes = salas.map((sala) => sala.user_id);
