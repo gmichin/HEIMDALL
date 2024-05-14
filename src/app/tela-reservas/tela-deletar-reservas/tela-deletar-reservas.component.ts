47ad30cd0937e1b91b0c8c71bab03bc2dae2bfee
Gustavo <gmassamichi@gmail.com>
2024-05-13 22:13:33 -0300

    save

--- src/app/tela-reservas/tela-deletar-reservas/tela-deletar-reservas.component.ts
+++ src/app/tela-reservas/tela-deletar-reservas/tela-deletar-reservas.component.ts
@@ -48,7 +48,7 @@ export class TelaDeletarReservasComponent {
     });
     
     this.salaDataService.salaData$.subscribe((salas) => {
-      const salasFiltradas = salas/* .filter((sala) => this.idSala.includes(sala._id)) */;
+      const salasFiltradas = salas.filter((sala) => this.idSala===sala._id);
       console.log(salasFiltradas);
       this.numeroSala = salasFiltradas.map((sala) => sala.number);
     });
