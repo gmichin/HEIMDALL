89b79f2e306886681e4ba4e2e182fbea51b0a149
Gustavo <gmassamichi@gmail.com>
2024-05-15 23:08:44 -0300

    save

--- src/app/tela-reservas/tela-reservas-feitas/tela-reservas-feitas.component.ts
+++ src/app/tela-reservas/tela-reservas-feitas/tela-reservas-feitas.component.ts
@@ -70,8 +70,6 @@ export class TelaReservasFeitasComponent {
         this.salaDataService.teacherData$.subscribe((professores) => {
             this.professores = professores;
         });
-        console.log(this.professores);
-        console.log(this.salas);
         const substituicoesPromises = this.salas.map(async (reserva) => {
             const professorCorrespondente = this.professores.find((prof) => prof._id === reserva.user_id);
             if (professorCorrespondente) {
