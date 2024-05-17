e734d8d78499e870f81e485c466e917431cef4e3
Gustavo <gmassamichi@gmail.com>
2024-05-16 21:48:08 -0300

    save

--- src/app/tela-reservas/tela-deletar-reservas/tela-deletar-reservas.component.ts
+++ src/app/tela-reservas/tela-deletar-reservas/tela-deletar-reservas.component.ts
@@ -122,7 +122,7 @@ export class TelaDeletarReservasComponent {
     this.todasReservas.forEach(reserva => {
       if (this.escolha === "numero") {
         const professorName = this.teacherFiltrado.find(teacher => reserva.user_id === teacher._id);
-        console.log(professorName);
+        console.log(professorName.name);
 
         this.salaName = this.salasFiltradas.find(sala => sala.number === selectedValue);
         if (this.salaName && reserva.room_id === this.salaName._id) {
