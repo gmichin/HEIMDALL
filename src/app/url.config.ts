098eb95c90ba7bf36e00771b6c3d2abd735a4796
Gustavo <gmassamichi@gmail.com>
2024-05-20 19:37:37 -0300

    save

--- src/app/url.config.ts
+++ src/app/url.config.ts
@@ -1,7 +1,7 @@
 export const url_config = Object.freeze({
-  url_registration: 'http://40.65.202.211:4203/registration',
-  url_user: 'http://40.65.202.211:4203/users',
-  url_instituition: 'http://40.65.202.211:4203/instituition',
-  url_course: 'http://40.65.202.211:4203/course',
-  url_roles: 'http://40.65.202.211:4203/roles',
+  url_registration: 'http://52.232.204.226:3000/registration',
+  url_user: 'http://52.232.204.226:3000/users',
+  url_instituition: 'http://52.232.204.226:3000/instituition',
+  url_course: 'http://52.232.204.226:3000/course',
+  url_roles: 'http://52.232.204.226:3000/roles',
 });
