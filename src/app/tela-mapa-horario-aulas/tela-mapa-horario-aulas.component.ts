0daa5cd3df011fc528630888749f12620e412abc
Gustavo <gmassamichi@gmail.com>
2024-05-20 20:42:45 -0300

    save

--- src/app/tela-mapa-horario-aulas/tela-mapa-horario-aulas.component.ts
+++ src/app/tela-mapa-horario-aulas/tela-mapa-horario-aulas.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
 import { RegisterUserResponse } from '../models/register.models';
 import { SessionService } from './../services/session.service';
+import { SalaDataService } from '../services/sala-data.service';
 
+interface Reserva {
+  _id: string;
+  room_id: string;
+  user_id: string;
+  class_id: string;
+  start_time: string;
+  end_time: string;
+  __v: number;
+}
 @Component({
   selector: 'app-tela-mapa-horario-aulas',
   templateUrl: './tela-mapa-horario-aulas.component.html',
@@ -9,19 +20,28 @@ import { SessionService } from './../services/session.service';
 })
 export class TelaMapaHorarioAulasComponent {
   public dataUser = <RegisterUserResponse>this.sessionService.getSessionData('user').retorno;
-
   public id = this.dataUser._id;
-  public name = this.dataUser.name;
-  public email = this.dataUser.email;
-  public class = this.dataUser.class;
+  public reservas: Reserva[] = [];
+  public userReservas: Reserva[] = [];
 
   constructor(
-    private sessionService: SessionService
+    private sessionService: SessionService,
+    private salaDataService: SalaDataService,
+    private datePipe: DatePipe
   ){
-    console.log(this.id);
-    console.log(this.name);
-    console.log(this.email);
-    console.log(this.class);
-    console.log(this.dataUser);
+    this.dataUser = this.sessionService.getSessionData('user').retorno as RegisterUserResponse;
+    this.salaDataService.salaReservaData$.subscribe(reservas => {
+      this.reservas = reservas;
+      this.filterUserReservas();
+    });
+  }
+
+  filterUserReservas() {
+    this.userReservas = this.reservas.filter(reserva => reserva.user_id === this.dataUser._id);
+    console.log(this.userReservas);
+  }
+
+  formatDate(date: string): string {
+    return this.datePipe.transform(new Date(date), 'dd MMM yyyy HH:mm') || date;
   }
 }
