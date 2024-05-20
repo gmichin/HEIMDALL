18da419e66f89e19194ed12a0e65409508fed52d
Gustavo <gmassamichi@gmail.com>
2024-05-20 20:14:48 -0300

    save

--- src/app/tela-mapa-horario-aulas/tela-mapa-horario-aulas.component.ts
+++ src/app/tela-mapa-horario-aulas/tela-mapa-horario-aulas.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { RegisterUserResponse } from '../models/register.models';
+import { SessionService } from './../services/session.service';
 
 @Component({
   selector: 'app-tela-mapa-horario-aulas',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './tela-mapa-horario-aulas.component.scss'
 })
 export class TelaMapaHorarioAulasComponent {
+  public dataUser = <RegisterUserResponse>this.sessionService.getSessionData('user').retorno;
 
+  public id = this.dataUser._id;
+  public name = this.dataUser.name;
+  public email = this.dataUser.email;
+
+  constructor(
+    private sessionService: SessionService
+  ){
+    console.log(this.id);
+    console.log(this.name);
+    console.log(this.email);
+  }
 }
