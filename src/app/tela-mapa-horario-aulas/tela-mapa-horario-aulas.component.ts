b0e8a7191c0f2240a9b34c60d50bcbe735cc8707
Gustavo <gmassamichi@gmail.com>
2024-05-20 19:34:24 -0300

    save

---
+++ src/app/tela-mapa-horario-aulas/tela-mapa-horario-aulas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tela-mapa-horario-aulas',
+  standalone: true,
+  imports: [],
+  templateUrl: './tela-mapa-horario-aulas.component.html',
+  styleUrl: './tela-mapa-horario-aulas.component.scss'
+})
+export class TelaMapaHorarioAulasComponent {
+
+}
--- src/app/url.config.ts
+++ src/app/url.config.ts
@@ -1,7 +1,7 @@
 export const url_config = Object.freeze({
-  url_registration: 'http://localhost:3000/registration',
-  url_user: 'http://localhost:3000/users',
-  url_instituition: 'http://localhost:3000/instituition',
-  url_course: 'http://localhost:3000/course',
-  url_roles: 'http://localhost:3000/roles',
+  url_registration: 'http://40.65.202.211:4203/registration',
+  url_user: 'http://40.65.202.211:4203/users',
+  url_instituition: 'http://40.65.202.211:4203/instituition',
+  url_course: 'http://40.65.202.211:4203/course',
+  url_roles: 'http://40.65.202.211:4203/roles',
 });
