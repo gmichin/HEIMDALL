83ca72a1c868a44c111d2056083d26c72e61ffa5
gustavofalconi <gustavo123755@gmail.com>
2024-05-21 08:08:44 -0300

    Alterações telas de salas (Precisa ver a responsividade das tabelas de salas)

--- src/app/tela-salas/tela-permissao-salas/tela-permissao-salas.component.ts
+++ src/app/tela-salas/tela-permissao-salas/tela-permissao-salas.component.ts
@@ -1,10 +1,11 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { SalaDataService } from 'src/app/services/sala-data.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { TelaLoginCadastroComponent } from 'src/app/tela-login-cadastro/tela-login-cadastro.component';
 import { TelaSalasComponent } from 'src/app/tela-salas/tela-salas.component';
 import { TelaReservasComponent } from 'src/app/tela-reservas/tela-reservas.component';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 interface Sala {
   number: number;
@@ -14,49 +15,60 @@ interface Sala {
   computadores: number;
   capacidade: number;
   projetor: number;
-  status : string;
+  status: string;
 }
+
 @Component({
   selector: 'app-tela-permissao-salas',
   templateUrl: './tela-permissao-salas.component.html',
-  styleUrl: './tela-permissao-salas.component.scss'
+  styleUrls: ['./tela-permissao-salas.component.scss']
 })
-export class TelaPermissaoSalasComponent {
+export class TelaPermissaoSalasComponent implements OnInit {
+  isMobile = false;
   requests: any[] = [];
-  displayedColumns: string[] = ['accept','numero', 'cadeiras','mesas','cadeirasPorMesa','computadores','lousa','projetor','status','remove'];
-
+  displayedColumns: string[] = ['accept', 'numero', 'cadeiras', 'mesas', 'cadeirasPorMesa', 'computadores', 'lousa', 'projetor', 'status', 'remove'];
   dataSource = new MatTableDataSource<Sala>(this.requests);
+
   constructor(
+    private breakpointObserver: BreakpointObserver,
     public dialog: MatDialog,
     private salaDataService: SalaDataService
-  ){    
+  ) {}
+
+  ngOnInit() {
+    // Observa mudanças no tamanho da tela para detectar se é um dispositivo móvel
+    this.breakpointObserver.observe([Breakpoints.Handset]).subscribe(result => {
+      this.isMobile = result.matches;
+    });
+
+    // Inscreve-se no serviço de dados para obter as solicitações de salas
     this.salaDataService.salasRequestData$.subscribe((salas) => {
       this.requests = salas;
-      this.dataSource.data = this.requests; 
+      this.dataSource.data = this.requests;
     });
   }
+
   openLoginSignUp() {
     const dialogRef = this.dialog.open(TelaLoginCadastroComponent);
-
     dialogRef.afterClosed().subscribe((result) => {
       console.log(`Dialog result: ${result}`);
     });
   }
+
   openReservas() {
     const dialogRef = this.dialog.open(TelaReservasComponent);
-
     dialogRef.afterClosed().subscribe((result) => {
       console.log(`Dialog result: ${result}`);
     });
   }
+
   openSalas() {
     const dialogRef = this.dialog.open(TelaSalasComponent);
-
     dialogRef.afterClosed().subscribe((result) => {
       console.log(`Dialog result: ${result}`);
     });
   }
-  
+
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
@@ -64,12 +76,11 @@ export class TelaPermissaoSalasComponent {
 
   acceptRow(sala: Sala) {
     const index = this.requests.findIndex(item => item === sala);
-    
     if (index !== -1) {
       this.requests.splice(index, 1);
-      this.dataSource.data = [...this.requests]; 
+      this.dataSource.data = [...this.requests];
     }
-  
+
     const acceptedData = [{
       "number": sala.number,
       "chairs": sala.cadeiras,
@@ -80,16 +91,15 @@ export class TelaPermissaoSalasComponent {
       "projectors": sala.projetor,
       "status": sala.status
     }];
-  
+
     console.log(acceptedData);
   }
 
-  removeRow(sala: Sala){
+  removeRow(sala: Sala) {
     const index = this.requests.findIndex(item => item === sala);
-    
     if (index !== -1) {
       this.requests.splice(index, 1);
-      this.dataSource.data = [...this.requests]; 
+      this.dataSource.data = [...this.requests];
     }
   }
 }
