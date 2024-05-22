7a13816af764c27055e4a9d628bb08f60cf05ef7
Gustavo <gmassamichi@gmail.com>
2024-05-22 01:10:50 -0300

    save

--- src/app/tela-mapa-horario-aulas/tela-mapa-horario-aulas.component.ts
+++ src/app/tela-mapa-horario-aulas/tela-mapa-horario-aulas.component.ts
@@ -1,23 +1,60 @@
-import { Component } from '@angular/core';
-import { DatePipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RegisterUserResponse } from '../models/register.models';
 import { SessionService } from './../services/session.service';
 import { SalaDataService } from '../services/sala-data.service';
+
+class Reservation {
+  _id: string = '';
+  room_id: string = '';
+  user_id: string = '';
+  class_id: string = '';
+  start_time: string = '';
+  end_time: string = '';
+  __v: number = 0;
+}
+
+interface ScheduleSlot {
+  date: Date;
+  classId: string;
+}
 
 @Component({
   selector: 'app-tela-mapa-horario-aulas',
   templateUrl: './tela-mapa-horario-aulas.component.html',
   styleUrl: './tela-mapa-horario-aulas.component.scss'
 })
-export class TelaMapaHorarioAulasComponent {
+export class TelaMapaHorarioAulasComponent implements OnInit{
   public dataUser = <RegisterUserResponse>this.sessionService.getSessionData('user').retorno;
 
   public id = this.dataUser._id;
 
+  public reservations: any[] = [];
+  public userReservations: any[] = [];
+
+  public schedule: ScheduleSlot[] = [];
+
   constructor(
     private sessionService: SessionService,
     private salaDataService: SalaDataService
-  ){
-    console.log(this.id);
+  ) { }
+
+  ngOnInit() {
+    this.salaDataService.carregarDadosSalasReservadas().subscribe(data => {
+      this.reservations = data;
+      this.filterUserReservations();
+    });
+  }
+
+  filterUserReservations() {
+    this.userReservations = this.reservations.filter(reservation => reservation.user_id === this.id);
+    this.processReservations();
+  }
+
+  processReservations() {
+    this.schedule = this.userReservations.map(reservation => ({
+      date: new Date(reservation.start_time),
+      classId: reservation.class_id
+    }));
+    console.log(this.schedule);
   }
 }
