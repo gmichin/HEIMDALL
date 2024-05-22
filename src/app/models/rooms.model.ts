f7838ac6074130bbd9d2095c46bb1b253277d22c
Érico Vinícius <ericovinicius333@gmail.com>
2024-05-21 22:01:12 -0300

    fix: subindo pra teste

--- src/app/models/rooms.model.ts
+++ src/app/models/rooms.model.ts
@@ -1,7 +1,26 @@
-export class RoomsModelResponse {
-  name!: string;
-  seats!: number;
-  description!: string;
-  teacher!: string;
-  schedule!: string;
+export class RoomsModel {
+  number!: number;
+  chairs!: number;
+  tables!: number;
+  chairByTables!: number;
+  computers!: number;
+  capacity!: number;
+  projectors!: number;
+  course_id!: string;
+  instituition_id!: string;
+  _id!: string
+  status!: 'DISPONIVEL' | 'OCUPADA';
+  
+  constructor(data: Required<RoomsModel>) {
+    this.number = data.number
+    this.chairs = data.chairs
+    this.tables = data.tables
+    this.chairByTables = data.chairByTables
+    this.computers = data.computers
+    this.capacity = data.capacity
+    this.projectors = data.projectors
+    this.course_id = data.course_id
+    this.instituition_id = data.instituition_id
+    this.status = data.status ?? 'DISPONIVEL'
+  }
 }
