704e3e2a8d929bc3ed74d142ba50db858a11da74
Gustavo <gmassamichi@gmail.com>
2024-05-23 17:02:23 -0300

    save

--- src/app/tela-mapa-horario-aulas/tela-mapa-horario-aulas.component.ts
+++ src/app/tela-mapa-horario-aulas/tela-mapa-horario-aulas.component.ts
@@ -3,7 +3,7 @@ import { RegisterUserResponse } from '../models/register.models';
 import { SessionService } from './../services/session.service';
 import { SalaDataService } from '../services/sala-data.service';
 import { forkJoin } from 'rxjs';
-import { eachHourOfInterval, parse, format, addHours } from 'date-fns';
+import { eachHourOfInterval, parse, addHours } from 'date-fns';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { TelaPerfilComponent } from 'src/app/tela-perfil/tela-perfil.component';
@@ -20,7 +20,7 @@ class Reservation {
 }
 
 interface ScheduleSlot {
-  date: string;
+  date: Date;
   classId: string;
   roomId: string;
 }
@@ -69,24 +69,26 @@ export class TelaMapaHorarioAulasComponent implements OnInit {
   }
 
   processReservations() {
+    const formatString = 'dd MMM d yyyy HH:mm:ss XXX'; // Format of the original string
+
     this.schedule = this.userReservations.flatMap(reservation => {
-      const start = new Date(reservation.start_time);
-      const end = new Date(reservation.end_time);
-      
+      const start = parse(reservation.start_time, formatString, new Date());
+      const end = parse(reservation.end_time, formatString, new Date());
+
       console.log(`Start Time (original): ${reservation.start_time}`);
       console.log(`End Time (original): ${reservation.end_time}`);
       console.log(`Start Time (Date object): ${start}`);
       console.log(`End Time (Date object): ${end}`);
-      
-      const slots = eachHourOfInterval({ start, end: new Date(end.getTime() - 1) }).map(date => ({
-        date: date.toISOString(),
+
+      const slots = eachHourOfInterval({ start, end: addHours(end, -1) }).map(date => ({
+        date,
         classId: reservation.class_id,
         roomId: reservation.room_id
       }));
 
       // Adicionar a última hora
       slots.push({
-        date: end.toISOString(),
+        date: end,
         classId: reservation.class_id,
         roomId: reservation.room_id
       });
@@ -98,7 +100,7 @@ export class TelaMapaHorarioAulasComponent implements OnInit {
 
   getReservation(day: string, hour: number): string {
     const reservation = this.schedule.find(slot => {
-      const date = new Date(slot.date);
+      const date = slot.date;
       return date.getHours() === hour && this.daysOfWeek[date.getDay() - 1] === day;
     });
     if (reservation) {
