825fe1395d76cfb6d46c2464c4b62a27c42b9848
Gustavo <gmassamichi@gmail.com>
2024-05-23 16:51:35 -0300

    save

--- src/app/tela-mapa-horario-aulas/tela-mapa-horario-aulas.component.ts
+++ src/app/tela-mapa-horario-aulas/tela-mapa-horario-aulas.component.ts
@@ -3,7 +3,7 @@ import { RegisterUserResponse } from '../models/register.models';
 import { SessionService } from './../services/session.service';
 import { SalaDataService } from '../services/sala-data.service';
 import { forkJoin } from 'rxjs';
-import { eachHourOfInterval } from 'date-fns';
+import { eachHourOfInterval, parse, formatISO } from 'date-fns';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { TelaPerfilComponent } from 'src/app/tela-perfil/tela-perfil.component';
@@ -70,8 +70,8 @@ export class TelaMapaHorarioAulasComponent implements OnInit {
 
   processReservations() {
     this.schedule = this.userReservations.flatMap(reservation => {
-      const start = new Date(reservation.start_time);
-      const end = new Date(reservation.end_time);
+      const start = parse(reservation.start_time, "dd LLL d yyyy HH:mm:ss X", new Date());
+      const end = parse(reservation.end_time, "dd LLL d yyyy HH:mm:ss X", new Date());
 
       console.log(`Start Time (original): ${reservation.start_time}`);
       console.log(`End Time (original): ${reservation.end_time}`);
