b8d859ec2a59348970537b69deea036f66d2fcd7
gustavofalconi <gustavo123755@gmail.com>
2024-09-24 19:30:50 -0300

    mudança nas urls do backend

--- src/app/url.config.ts
+++ src/app/url.config.ts
@@ -1,10 +1,10 @@
 export const url_config = Object.freeze({
-  url_registration: 'http://52.232.204.226:3000/registration',
-  url_user: 'http://52.232.204.226:3000/users',
-  url_instituition: 'http://52.232.204.226:3000/instituition',
-  url_course: 'http://52.232.204.226:3000/course',
-  url_roles: 'http://52.232.204.226:3000/roles',
-  url_class: 'http://52.232.204.226:3000/class',
-  url_room: 'http://52.232.204.226:3000/room',
-  url_reserve: 'http://52.232.204.226:3000/reserve',
+  url_registration: 'http://10.0.0.5:3000/registration',
+  url_user: 'http://10.0.0.5:3000/users',
+  url_instituition: 'http://10.0.0.5:3000/instituition',
+  url_course: 'http://10.0.0.5:3000/course',
+  url_roles: 'http://10.0.0.5:3000/roles',
+  url_class: 'http://10.0.0.5:3000/class',
+  url_room: 'http://10.0.0.5:3000/room',
+  url_reserve: 'http://10.0.0.5:3000/reserve',
 });
