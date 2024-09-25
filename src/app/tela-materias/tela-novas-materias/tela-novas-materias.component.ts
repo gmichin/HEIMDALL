ee66e8d6d67c3a6c1707293e930c99114631d724
Gustavo <gmassamichi@gmail.com>
2024-09-24 22:12:23 -0300

    save

--- src/app/tela-materias/tela-novas-materias/tela-novas-materias.component.ts
+++ src/app/tela-materias/tela-novas-materias/tela-novas-materias.component.ts
@@ -23,16 +23,18 @@ import { RoleId } from 'src/app/models/role.model';
 @Component({
   selector: 'app-tela-novas-materias',
   templateUrl: './tela-novas-materias.component.html',
-  styleUrl: './tela-novas-materias.component.scss'
+  styleUrl: './tela-novas-materias.component.scss',
 })
 export class TelaNovasMateriasComponent implements OnInit {
   public resgiterForm: FormGroup;
   public courses: CourseModelResponse[] = [];
   public teachersList: RegisterUserResponse[] = [];
-  public classToEdit!: {valid: boolean, class: ClassModel};
+  public classToEdit!: { valid: boolean; class: ClassModel };
 
-  newSala: any[] = [];  
-  public dataUser = <RegisterUserResponse>this.sessionService.getSessionData('user').retorno;
+  newSala: any[] = [];
+  public dataUser = <RegisterUserResponse>(
+    this.sessionService.getSessionData('user').retorno
+  );
   public id = this.dataUser._id;
   public role = '';
 
@@ -47,69 +49,95 @@ export class TelaNovasMateriasComponent implements OnInit {
     private classService: ClassService,
     private router: Router,
     private reload: ReloadService
-  ) {  
-    switch(this.dataUser.role){
-    case RoleId.ADM:
-      this.role = 'Administrador';
-      break;
-    case RoleId.PROFESSOR:
-      this.role = 'Professor';
-      break;
-    case RoleId.ALUNO:
-      this.role = 'Aluno';
-      break;
-  }
+  ) {
+    switch (this.dataUser.role) {
+      case RoleId.ADM:
+        this.role = 'Administrador';
+        break;
+      case RoleId.PROFESSOR:
+        this.role = 'Professor';
+        break;
+      case RoleId.ALUNO:
+        this.role = 'Aluno';
+        break;
+    }
     this.classToEdit = this.classService.getClassToEdit();
 
-    this.resgiterForm = this.fb.group(
-      {
-        name: [this.classToEdit.valid ? this.classToEdit.class.name: '', [Validators.required]],
-        description: [this.classToEdit.valid ? this.classToEdit.class.description : '', [Validators.required]],
-        teachers_id: [this.classToEdit.valid ? this.classToEdit.class.teachers_id : [], [Validators.required]],
-        course_id: [this.classToEdit.valid ? this.classToEdit.class.course_id : '', [Validators.required]],
-      }
-    );
+    this.resgiterForm = this.fb.group({
+      name: [
+        this.classToEdit.valid ? this.classToEdit.class.name : '',
+        [Validators.required],
+      ],
+      description: [
+        this.classToEdit.valid ? this.classToEdit.class.description : '',
+        [Validators.required],
+      ],
+      teachers_id: [
+        this.classToEdit.valid ? this.classToEdit.class.teachers_id : [],
+        [Validators.required],
+      ],
+      course_id: [
+        this.classToEdit.valid ? this.classToEdit.class.course_id : '',
+        [Validators.required],
+      ],
+    });
   }
   ngOnInit(): void {
     this.internsService.getAllTeachers().subscribe({
-      next: res => {
+      next: (res) => {
         this.teachersList = res;
-        if(res.length == 0) {
-          this.snackBar.open('Ocorreu um erro durante a busca dos professores, por favor, tente novamente mais tarde.', '', {
-            duration: 4000,
-          });
+        if (res.length == 0) {
+          this.snackBar.open(
+            'Ocorreu um erro durante a busca dos professores, por favor, tente novamente mais tarde.',
+            '',
+            {
+              duration: 4000,
+            }
+          );
         }
       },
-      error: err => {
-        this.snackBar.open('Ocorreu um erro durante a busca dos professores, por favor, tente novamente mais tarde.', '', {
-          duration: 2000,
-        });
-      }
+      error: (err) => {
+        this.snackBar.open(
+          'Ocorreu um erro durante a busca dos professores, por favor, tente novamente mais tarde.',
+          '',
+          {
+            duration: 2000,
+          }
+        );
+      },
     });
 
     this.courseService.getAllCourses().subscribe({
-      next: res => {
+      next: (res) => {
         this.courses = res;
-        if(res.length == 0) {
-          this.snackBar.open('Ocorreu um erro durante a busca dos cursos, por favor, tente novamente mais tarde.', '', {
-            duration: 4000,
-          });
+        if (res.length == 0) {
+          this.snackBar.open(
+            'Ocorreu um erro durante a busca dos cursos, por favor, tente novamente mais tarde.',
+            '',
+            {
+              duration: 4000,
+            }
+          );
         }
       },
-      error: err => {
-        this.snackBar.open('Ocorreu um erro durante a busca dos cursos, por favor, tente novamente mais tarde.', '', {
-          duration: 2000,
-        });
-      }
+      error: (err) => {
+        this.snackBar.open(
+          'Ocorreu um erro durante a busca dos cursos, por favor, tente novamente mais tarde.',
+          '',
+          {
+            duration: 2000,
+          }
+        );
+      },
     });
   }
 
-  goBack(){
-    if(this.role=="Administrador") this.router.navigate(['/home-adm']);
-    else if(this.role=="Professor") this.router.navigate(['/home-teacher']);
-    else if(this.role=="Aluno") this.router.navigate(['/home-student']);
+  goHome() {
+    if (this.role == 'Administrador') this.router.navigate(['/home-adm']);
+    else if (this.role == 'Professor') this.router.navigate(['/home-teacher']);
+    else if (this.role == 'Aluno') this.router.navigate(['/home-student']);
   }
-  logout(){
+  logout() {
     this.router.navigate(['/']);
   }
 
@@ -123,9 +151,9 @@ export class TelaNovasMateriasComponent implements OnInit {
   }
 
   public redirectHomeAdm() {
-    this.reload.reoladPage(['redirecionar'])
+    this.reload.reoladPage(['redirecionar']);
   }
-  
+
   private dialogCloseSubs() {
     this.reload.reoladPage(['redirecionar']);
   }
@@ -152,47 +180,54 @@ export class TelaNovasMateriasComponent implements OnInit {
   }
 
   public save() {
-    const instituition_id = this.sessionService.getSessionData<string>(
-      'idInstitution'
-    ).retorno;
+    const instituition_id =
+      this.sessionService.getSessionData<string>('idInstitution').retorno;
 
     const materia = new ClassModel({
       name: this.resgiterForm.get('name')?.value,
       description: this.resgiterForm.get('description')?.value,
       teachers_id: this.resgiterForm.get('teachers_id')?.value,
       course_id: this.resgiterForm.get('course_id')?.value,
-      _id:'',
+      _id: '',
     });
-    
-    if(this.classToEdit.valid) {
+
+    if (this.classToEdit.valid) {
       materia._id = this.classToEdit.class._id;
       this.classService.updateClass(materia).subscribe({
-        next: res => {
+        next: (res) => {
           this.snackBar.open('Atualizado com sucesso!', '', {
             duration: 4000,
           });
           this.reloadService.reoladPage(['tela-novas-materias']);
         },
-        error: err => {
-          this.snackBar.open('Ocorreu um erro durante a atualização, por favor, tente novamente mais tarde.', '', {
-            duration: 4000,
-          });
-        }
-      })
+        error: (err) => {
+          this.snackBar.open(
+            'Ocorreu um erro durante a atualização, por favor, tente novamente mais tarde.',
+            '',
+            {
+              duration: 4000,
+            }
+          );
+        },
+      });
       return;
     }
     this.classService.createClass(materia).subscribe({
-      next: res => {
+      next: (res) => {
         this.snackBar.open('Cadastrado com sucesso!', '', {
           duration: 4000,
         });
         this.reloadService.reoladPage(['tela-novas-materias']);
       },
-      error: err => {
-        this.snackBar.open('Ocorreu um erro durante o cadastro, por favor, tente novamente mais tarde.', '', {
-          duration: 4000,
-        });
-      }
-    })
+      error: (err) => {
+        this.snackBar.open(
+          'Ocorreu um erro durante o cadastro, por favor, tente novamente mais tarde.',
+          '',
+          {
+            duration: 4000,
+          }
+        );
+      },
+    });
   }
 }
