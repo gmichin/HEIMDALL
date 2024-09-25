bf2c7bf75b04acc8ad9328888799dbd0a7351db5
gustavofalconi <gustavo123755@gmail.com>
2024-09-24 23:43:33 -0300

    tirando porta 3000

--- src/app/url.config.ts
+++ src/app/url.config.ts
@@ -1,10 +1,10 @@
 export const url_config = Object.freeze({
-  url_registration: 'https://10.0.0.5:3000/registration',
-  url_user: 'https://10.0.0.5:3000/users',
-  url_instituition: 'https://10.0.0.5:3000/instituition',
-  url_course: 'https://10.0.0.5:3000/course',
-  url_roles: 'https://10.0.0.5:3000/roles',
-  url_class: 'https://10.0.0.5:3000/class',
-  url_room: 'https://10.0.0.5:3000/room',
-  url_reserve: 'https://10.0.0.5:3000/reserve',
+  url_registration: 'https://10.0.0.5/registration',
+  url_user: 'https://10.0.0.5/users',
+  url_instituition: 'https://10.0.0.5/instituition',
+  url_course: 'https://10.0.0.5/course',
+  url_roles: 'https://10.0.0.5/roles',
+  url_class: 'https://10.0.0.5/class',
+  url_room: 'https://10.0.0.5/room',
+  url_reserve: 'https://10.0.0.5/reserve',
 });
