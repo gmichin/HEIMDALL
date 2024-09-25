62d8427c13391d83f1c93116baf1e256299afef2
gustavofalconi <gustavo123755@gmail.com>
2024-09-24 23:03:38 -0300

    mudança urls http -> https

--- src/app/url.config.ts
+++ src/app/url.config.ts
@@ -1,10 +1,10 @@
 export const url_config = Object.freeze({
-  url_registration: 'http://10.0.0.5:3000/registration',
-  url_user: 'http://10.0.0.5:3000/users',
-  url_instituition: 'http://10.0.0.5:3000/instituition',
-  url_course: 'http://10.0.0.5:3000/course',
-  url_roles: 'http://10.0.0.5:3000/roles',
-  url_class: 'http://10.0.0.5:3000/class',
-  url_room: 'http://10.0.0.5:3000/room',
-  url_reserve: 'http://10.0.0.5:3000/reserve',
+  url_registration: 'https://10.0.0.5:3000/registration',
+  url_user: 'https://10.0.0.5:3000/users',
+  url_instituition: 'https://10.0.0.5:3000/instituition',
+  url_course: 'https://10.0.0.5:3000/course',
+  url_roles: 'https://10.0.0.5:3000/roles',
+  url_class: 'https://10.0.0.5:3000/class',
+  url_room: 'https://10.0.0.5:3000/room',
+  url_reserve: 'https://10.0.0.5:3000/reserve',
 });
