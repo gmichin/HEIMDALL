fc9209c2db02c94d85884381c923731a9a85cf94
Gustavo <gmassamichi@gmail.com>
2024-10-25 15:56:24 -0300

    tela de perfil ccompletada

--- src/app/tela-perfil/tela-perfil.component.ts
+++ src/app/tela-perfil/tela-perfil.component.ts
@@ -6,6 +6,7 @@ import { SessionService } from './../services/session.service';
 import { CadastroService } from '../services/cadastros.service';
 import { AlunoModel } from '../models/aluno.model';
 import { ProfessorModel } from '../models/professor.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tela-perfil',
@@ -26,38 +27,50 @@ export class TelaPerfilComponent implements OnInit {
   public nomeProfessor = this.dataProfessorAdm.nome;
   public emailProfessor = this.dataProfessorAdm.email;
 
-  public form: FormGroup;
+  public form!: FormGroup;
 
   constructor(
     private sessionService: SessionService,
     private snackBar: MatSnackBar,
     private fb: FormBuilder,
     public dialogRef: MatDialogRef<TelaPerfilComponent>,
+    private router: Router,
     public cadastro: CadastroService
   ) {
-    switch (this.dataProfessorAdm.adm) {
-      case true:
-        this.tipoUsuario = 'Administrador';
-        break;
-      case false:
-        this.tipoUsuario = 'Professor';
-        break;
+    if (this.dataProfessorAdm.adm) {
+      this.tipoUsuario = 'Administrador';
+      this.criarFormulario(
+        this.dataProfessorAdm.email,
+        this.dataProfessorAdm.nome,
+        this.tipoUsuario
+      );
+    } else if (!this.dataProfessorAdm.adm) {
+      this.tipoUsuario = 'Professor';
+      this.criarFormulario(
+        this.dataProfessorAdm.email,
+        this.dataProfessorAdm.nome,
+        this.tipoUsuario
+      );
+    } else if (this.dataAluno) {
+      this.tipoUsuario = 'Aluno';
+      this.criarFormulario(
+        this.dataAluno.email,
+        this.dataAluno.nome,
+        this.tipoUsuario
+      );
     }
-    if (this.dataAluno) this.tipoUsuario = 'Aluno';
-
-    this.form = this.fb.group({
-      email: [this.dataAluno.email, [Validators.required, this.emailValidator]],
-      name: [this.dataAluno.nome, [Validators.required]],
-      role: [
-        this.tipoUsuario == 'Aluno' ? 'Aluno' : 'Professor',
-        [Validators.required],
-      ],
-    });
   }
 
   ngOnInit() {}
 
-  public save() {
+  private criarFormulario(email: string, nome: string, role: string) {
+    this.form = this.fb.group({
+      email: [email, [Validators.required, this.emailValidator]],
+      nome: [nome, [Validators.required]],
+      role: [role, [Validators.required]],
+    });
+  }
+  public salvar() {
     if (this.form.invalid) {
       this.snackBar.open('Por favor, revise os campos.', '', {
         duration: 1000,
@@ -66,14 +79,20 @@ export class TelaPerfilComponent implements OnInit {
     }
 
     if (this.tipoUsuario == 'Aluno') {
+      console.log(this.dataAluno);
       this.dataAluno.email = this.form.get('email')?.value;
-      this.dataAluno.nome = this.form.get('name')?.value;
+      this.dataAluno.nome = this.form.get('nome')?.value;
       this.cadastro.atualizarAluno(this.dataAluno).subscribe({
         next: () => {
           this.snackBar.open('Dados Atualizados com sucesso.', '', {
             duration: 1000,
           });
-          this.dialogRef.close();
+          this.router
+            .navigateByUrl('/', { skipLocationChange: true })
+            .then(() => {
+              this.router.navigate(['home-adm']);
+            });
+          this.dialogRef.close('close');
         },
         error: () => {
           this.snackBar.open('Ocorreu um erro durante sua solicitação.', '', {
@@ -82,15 +101,24 @@ export class TelaPerfilComponent implements OnInit {
           this.dialogRef.close();
         },
       });
-    } else if (this.tipoUsuario == 'Professor' || this.tipoUsuario == 'Adm') {
+    } else if (
+      this.tipoUsuario == 'Professor' ||
+      this.tipoUsuario == 'Administrador'
+    ) {
+      console.log(this.dataProfessorAdm);
       this.dataProfessorAdm.email = this.form.get('email')?.value;
-      this.dataProfessorAdm.nome = this.form.get('name')?.value;
+      this.dataProfessorAdm.nome = this.form.get('nome')?.value;
       this.cadastro.atualizarProfessor(this.dataProfessorAdm).subscribe({
         next: () => {
           this.snackBar.open('Dados Atualizados com sucesso.', '', {
             duration: 1000,
           });
-          this.dialogRef.close();
+          this.router
+            .navigateByUrl('/', { skipLocationChange: true })
+            .then(() => {
+              this.router.navigate(['home-adm']);
+            });
+          this.dialogRef.close('close');
         },
         error: () => {
           this.snackBar.open('Ocorreu um erro durante sua solicitação.', '', {
