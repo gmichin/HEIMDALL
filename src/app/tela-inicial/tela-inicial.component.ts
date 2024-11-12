c57bde6cee5bb0b99cf26674fa20df08500366d0
Gustavo <gmassamichi@gmail.com>
2024-11-12 17:46:10 -0300

    save

--- src/app/tela-inicial/tela-inicial.component.ts
+++ src/app/tela-inicial/tela-inicial.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { TelaLoginCadastroComponent } from '../tela-login-cadastro/tela-login-cadastro.component';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-tela-inicial',
   templateUrl: './tela-inicial.component.html',
   styleUrls: ['./tela-inicial.component.scss'],
 })
 export class TelaInicialComponent {
-  constructor(public dialog: MatDialog) {
+  constructor(public dialog: MatDialog, private router: Router) {
     sessionStorage.clear();
   }
 
@@ -17,5 +18,8 @@ export class TelaInicialComponent {
     dialogRef.afterClosed().subscribe((result) => {
       console.log(`Dialog result: ${result}`);
     });
+  }
+  openSuporteAjuda() {
+    this.router.navigate(['tela-suporte-ajuda']);
   }
 }
--- src/app/tela-suporte-ajuda/tela-suporte-ajuda.component.ts
+++ src/app/tela-suporte-ajuda/tela-suporte-ajuda.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { TelaLoginCadastroComponent } from '../tela-login-cadastro/tela-login-cadastro.component';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-tela-suporte-ajuda',
   templateUrl: './tela-suporte-ajuda.component.html',
   styleUrl: './tela-suporte-ajuda.component.scss',
 })
 export class TelaSuporteAjudaComponent {
-  constructor(public dialog: MatDialog) {
+  constructor(public dialog: MatDialog, private router: Router) {
     sessionStorage.clear();
   }
 
@@ -17,5 +18,8 @@ export class TelaSuporteAjudaComponent {
     dialogRef.afterClosed().subscribe((result) => {
       console.log(`Dialog result: ${result}`);
     });
+  }
+  goBack() {
+    this.router.navigate(['inicial']);
   }
 }
